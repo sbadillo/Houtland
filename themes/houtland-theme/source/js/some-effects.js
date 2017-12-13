19018482d5300f8e22f4f3c6211cc1269977c489
serch <sbadillo@gmail.com>
2017-12-13 11:27:07 +0100

    video stops if scroll down, initial volume controlled by javascripy

--- themes/houtland-theme/source/js/some-effects.js
+++ themes/houtland-theme/source/js/some-effects.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  // set video iniciat volume
+  var vid = $('video').get(0);
+  vid.volume = 0.2;
+
 	// Appear on scroll navbar
 	// (this one is used in the main page only)
 	
@@ -16,14 +20,20 @@ $(document).ready(function() {
       $(function () {
         $(window).scroll(function () {
                 // set distance user needs to scroll before we fadeIn navbar
-                if ($(this).scrollTop() > 500) {
-                  $('.super-navbar').fadeIn(350);
+                if ($(this).scrollTop() > 300) { 
+
+                  // show navbar
+                  $('.super-navbar').fadeIn(0);
                   $('.super-navbar').addClass('d-flex');
+                  // and stop video
+                  $('video').get(0).pause();
+
                   // console.log('show');
                 } else {
                   $('.super-navbar').removeClass('d-flex');
-                  $('.super-navbar').fadeOut(10);
-                  // console.log('hide');
+                  $('.super-navbar').fadeOut(0);
+                  $('video').get(0).play();
+                  
                 }
             });
       });
@@ -36,27 +46,6 @@ $(document).ready(function() {
 	$(".lazy").lazyload({
 		effect          : "fadeIn"
 	});
-
-
-	// magnific popup
-	$('.mfp-iframe').magnificPopup({
-		type:'iframe',
-		iframe: {
-			markup: '<div class="mfp-iframe-scaler">'+
-			'<div class="mfp-close"></div>'+
-			'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
-
-          patterns: {
-          	gmaps: {
-          		index: '//maps.google.',
-          		src: '%id%&output=embed'
-          	}
-          },
-
-  		srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
-  	}
-  });
 
 });
 
