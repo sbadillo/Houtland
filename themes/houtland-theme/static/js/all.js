0598d99dea2b583044cff8540e9be7325893645a
serch <sbadillo@gmail.com>
2018-01-17 15:59:28 +0100

    added fake loading to tree page

--- themes/houtland-theme/static/js/all.js
+++ themes/houtland-theme/static/js/all.js
@@ -1181,36 +1181,28 @@ $(document).ready(function() {
     $('body').removeClass('fade-out');
   });
 
-
   var pathname = window.location.pathname; // Returns path only
-  console.log(pathname);
   
   // // Appear on scroll navbar
   // // (this one is used in the main page only)
-  // // hide super-navbar first (this one is used in the main page only)
-  // if (pathname == '/' || pathname == '/houtland/') {
+  
+  if (pathname == '/' || pathname == '/houtland/') {
 
-  //   // set video iniciat volume
-  //   var vid = $('video').get(0);
-  //   vid.volume = 0.2;
+    console.log("HELLO MAIN PAGE :)")
+    var elementPosition = $('#header_nav').offset();
 
-  //   // hide it first
-  //   $(".super-navbar").hide();
-  //   $('.super-navbar').removeClass('static-top').addClass('fixed-top');
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > elementPosition.top) {
+        $('#header_nav').addClass('fixed-top navbar-when-fixed', {duration:500});
+
+      } else {
+        $('#header_nav').removeClass('fixed-top navbar-when-fixed')
+      }
+    });
+
+  };
 
 
-  // };
-
-  var elementPosition = $('#header_nav').offset();
-
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > elementPosition.top) {
-      $('#header_nav').addClass('fixed-top navbar-when-fixed', {duration:500});
-
-    } else {
-      $('#header_nav').removeClass('fixed-top navbar-when-fixed')
-    }
-  });
 
 
 	// Use lazyload
@@ -1223,36 +1215,34 @@ $(document).ready(function() {
  // Instance the tour
 
 
+ $( "#support-button" ).click(function() {
 
-  
-  $( "#support-button" ).click(function() {
-
-      var tour = new Tour({
-      name: "tour",
-      smartPlacement: false,
-      steps: [
-      {
-        title:"Hi!",
-        content: "You can support by choosing your actions over here." ,
-        element: "#tour-target",
-        placement: 'top'
-      }
-      ],
-      template: 
-      '<div class="popover popover-dismiss border-full" role="tooltip"> \
-      <div class="arrow"></div> \
-      <div class="popover-data-content lead m-4">You can support by choosing your actions over here.</div>\
-      <div class="popover-navigation"><button class="btn btn-nocolor" data-role="end">Okay!</button></div> \
-      </div>',
-      container: "body",
-      keyboard: true,
-      debug: true,
-      backdrop: true,
-      backdropContainer: 'body',
-      backdropPadding: 20,
-      redirect: true,
-      orphan: true,
-    });
+  var tour = new Tour({
+    name: "tour",
+    smartPlacement: false,
+    steps: [
+    {
+      title:"Hi!",
+      content: "You can support by choosing your actions over here." ,
+      element: "#tour-target",
+      placement: 'top'
+    }
+    ],
+    template: 
+    '<div class="popover popover-dismiss border-full" role="tooltip"> \
+    <div class="arrow"></div> \
+    <div class="popover-data-content lead m-4">You can support by choosing your actions over here.</div>\
+    <div class="popover-navigation"><button class="btn btn-nocolor" data-role="end">Okay!</button></div> \
+    </div>',
+    container: "body",
+    keyboard: true,
+    debug: true,
+    backdrop: true,
+    backdropContainer: 'body',
+    backdropPadding: 20,
+    redirect: true,
+    orphan: true,
+  });
 
     // Initialize the tour
     tour.init();
@@ -1261,6 +1251,27 @@ $(document).ready(function() {
 
 
   });
+
+
+
+// =================  Applies to livestream page only: ============
+
+if (pathname == '/pages/tree01.html' || pathname == '/houtland/pages/tree01.html') {
+
+
+  $(".video-noclick").bind("contextmenu",function(){
+    return false;
+  });
+
+  var waittime = 4000 + Math.floor(Math.random() * 7000);
+  console.log("HELLO LIVETREE!", waittime);
+  
+  setTimeout(function(){
+    $(".loading").css('display', 'none');
+    $(".video-noclick").css('display','inline');
+  }, waittime);
+
+};
 
 
 
